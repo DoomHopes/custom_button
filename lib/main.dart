f5a28d73cfab772a89eb8bde126f4ebcdd781bf5
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-25 11:47:31 +0200

    some fix

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,38 @@ class MyApp extends StatelessWidget {
       title: 'Custom Button',
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Custom Button'),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: CustomButton(
-            text: 'Upload',
-            icon: Icon(Icons.upload_file),
-            color: Colors.red,
+          appBar: AppBar(
+            title: const Text('Custom Button'),
+            centerTitle: true,
           ),
-        ),
-      ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                CustomButton(
+                  text: 'Upload',
+                  icon: Icon(Icons.upload_file),
+                  color: Colors.blue,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomButton(
+                  text: 'Search',
+                  icon: Icon(Icons.search),
+                  color: Colors.purple,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomButton(
+                  text: 'Settings',
+                  icon: Icon(Icons.settings),
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
