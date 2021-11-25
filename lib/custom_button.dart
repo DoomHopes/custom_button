363228a665cdd51cdd5e1c9b3ac3ad9df18087ce
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-25 10:52:15 +0200

    some change

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -12,12 +12,65 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 60,
-      width: 200,
-      decoration: const BoxDecoration(
-        color: Colors.green,
-        borderRadius: BorderRadius.only(topRight: Radius.circular(90)),
+    return SizedBox(
+      width: 280,
+      height: 80,
+      child: Container(
+        color: Colors.black,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: 1,
+              top: 18,
+              child: Container(
+                height: 50,
+                width: 200,
+                child: Center(
+                  child: Text(
+                    text,
+                    style: const TextStyle(fontSize: 25),
+                  ),
+                ),
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.greenAccent,
+                      Colors.green,
+                    ],
+                  ),
+                  borderRadius:
+                      BorderRadius.only(topRight: Radius.circular(90)),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 17,
+              left: 53,
+              child: Container(
+                height: 55,
+                width: 55,
+                child: icon,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green[900],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 9,
+              left: 45,
+              child: Container(
+                height: 70,
+                width: 70,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
