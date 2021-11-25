53ae8b2176139c7c1ebd335420a3957c2f0417a7
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-25 10:34:47 +0200

    some change

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -12,42 +12,13 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Positioned(
-          width: 200,
-          height: 60,
-          child: Container(
-            child: Center(
-              child: Text(
-                text,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontSize: 25,
-                ),
-              ),
-            ),
-            decoration: const BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.only(topRight: Radius.circular(90)),
-            ),
-          ),
-        ),
-        Positioned(
-          left: 70,
-          height: 70,
-          width: 70,
-          child: Container(
-            child: icon,
-            decoration: const BoxDecoration(
-              color: Colors.red,
-              shape: BoxShape.circle,
-            ),
-          ),
-        ),
-      ],
+    return Container(
+      height: 60,
+      width: 200,
+      decoration: const BoxDecoration(
+        color: Colors.green,
+        borderRadius: BorderRadius.only(topRight: Radius.circular(90)),
+      ),
     );
   }
 }
